50dfe4ad7ac8b6e3d84d230a726607cd193b69d5
MechaelDodo <modod@bk.ru>
2018-05-23 14:11:34 +0300

    add exit button to Form2

--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
@@ -28,11 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(489, 428);
+            this.button2.MaximumSize = new System.Drawing.Size(165, 59);
+            this.button2.MinimumSize = new System.Drawing.Size(165, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 59);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(697, 523);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(713, 557);
+            this.MinimumSize = new System.Drawing.Size(713, 557);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
